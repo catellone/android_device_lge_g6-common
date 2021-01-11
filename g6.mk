1af861e0f526e2166bfd8f70c6d08d3334bcf9e8
Michael Bardoň <wolfovelp@gmail.com>
2021-01-12 00:21:08 +0100

    FP Changes

--- g6.mk
+++ g6.mk
@@ -38,7 +38,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.lucye
 
 # HDR
 PRODUCT_COPY_FILES += \
