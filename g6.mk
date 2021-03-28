9864c6c29a283d1f2897ff90055a86c624a7da99
Michael Bardoň <wolfovelp@gmail.com>
2021-03-28 23:24:01 +0200

    g6-common: Update Camera & Media blobs from V30B

--- g6.mk
+++ g6.mk
@@ -50,7 +50,9 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # Sensors
 PRODUCT_COPY_FILES += \
