542e8f76a2eac46222c2e0e3cb0025683ee84887
Michael Bardoň <wolfovelp@gmail.com>
2021-04-02 21:30:53 +0200

    g6-common: Update media_profiles from V30B

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -5,3 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # HDR
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qcom.hdr.config=/system/vendor/etc/hdr_tm_config.xml
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml
